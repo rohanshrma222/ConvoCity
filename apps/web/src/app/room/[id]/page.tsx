d19e6c8b5488f954ec673afac5924f0d1a193045
rohanshrma222 <hackerviruse04@gmail.com>
2026-03-18 20:22:12 +0530

    synced server

---
+++ apps/web/src/app/room/[id]/page.tsx
@@ -0,0 +1,9 @@
+import GameCanvas from "../../components/GameCanvas";
+
+export default function RoomPage() {
+  return (
+    <main className="w-full h-screen overflow-hidden">
+      <GameCanvas />
+    </main>
+  );
+}
